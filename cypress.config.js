9995d9d7f86d09d901c05648bf84a1190c8c0e7a
Oscar Castro <ocastrobeltran@gmail.com>
2024-08-29 13:20:08 -0500

    Junit config

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config
       // implement node event listeners here
     },
   },
   reporter: 'mocha-junit-reporter',
   reporterOptions: {
-    mochaFile: 'results/test-results-[hash].xml',
+    mochaFile: 'cypress/results/test-results-[hash].xml',
   },
 });
+module.exports.reporter = 'mochawesome';
